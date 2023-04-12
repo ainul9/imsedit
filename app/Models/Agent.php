7e3adbde28f8f68959305760fc21c71571d22038
ainul9 <ainulhayat1096@gmail.com>
2023-04-12 15:56:44 +0800

    first commit

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -10,4 +10,26 @@ class Agent extends Model
     use HasFactory;
 
     protected $table = 'agent';
+
+    protected $fillable = [
+        'usersID',
+        'agentName',
+        'agentCat',
+        'registrationNum',
+        'contact',
+        'address',
+        'city',
+        'postcode',
+        'state',
+        'country',
+        'remarks'
+    ];
+
+    protected $guarded = ['userID', 'agentName'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'usersID');
+    }
+
 }
