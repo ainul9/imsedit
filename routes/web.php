41e184c282aa9dceaddc2891238f9a7a70e1af5d
ainul9 <ainulhayat1096@gmail.com>
2023-04-14 15:48:59 +0800

    edit details

--- routes/web.php
+++ routes/web.php
@@ -35,11 +35,46 @@ Route::get('/edit_user/{id}', [App\Http\Controllers\backend\UsermanagementContro
 Route::post('/update_user/{id}', [App\Http\Controllers\backend\UsermanagementController::class,'UserUpdate']);
 Route::get('/delete_user/{id}', [App\Http\Controllers\backend\UsermanagementController::class,'UserDelete']);
 
-Route::get('list_agent', [App\Http\Controllers\backend\AgentController::class,'AgentList'])->name('agent.index');
-Route::get('/add_agent/{usersID}/{agentName}',[App\Http\Controllers\backend\AgentController::class,'AgentAdd'])->name('agentadd');
+Route::get('list_agent', [App\Http\Controllers\backend\AgentController::class,'AgentList']);
+Route::get('/add_agent/{usersID}/{agentName}',[App\Http\Controllers\backend\AgentController::class,'AgentAdd'])->name('backend.agent.create_agent');
 Route::post('/insert_agent/{usersID}', [App\Http\Controllers\backend\AgentController::class,'AgentInsert']);
-Route::get('/show_details/{usersID}', [App\Http\Controllers\backend\AgentController::class,'AgentShow']);
+Route::get('/show_details/{usersID}', [App\Http\Controllers\backend\AgentController::class,'AgentShow'])->name('backend.agent.show_details');
+// Route::get('/edit_agent/{usersID}', [App\Http\Controllers\backend\AgentController::class,'AgentEdit']);
 Route::get('/edit_agent/{agentID}', [App\Http\Controllers\backend\AgentController::class,'AgentEdit']);
-Route::post('/update_agent/{agentID}', [App\Http\Controllers\backend\AgentController::class,'AgentUpdate']);
+Route::POST('/update_agent/{agentID}', [App\Http\Controllers\backend\AgentController::class,'AgentUpdate']);
 Route::get('/delete_agent/{agentID}', [App\Http\Controllers\backend\AgentController::class,'AgentDelete']);
 
+
+
+
+
+
+// public function AgentInsert(Request $request, $usersID)
+// {
+//     $user = User::find($usersID);
+//     $data = [
+//         'usersID' => $user->id,
+//         'agentName' => $user->name,
+//         'agentCat' => $request->agentCat,
+//         'registrationNum' => $request->registrationNum,
+//         'contact' => $request->contact,
+//         'address' => $request->address,
+//         'city' => $request->city,
+//         'postcode' => $request->postcode,
+//         'state' => $request->state,
+//         'country' => $request->country,
+//         'remarks' => $request->remarks,
+//     ];
+//     $insert = DB::table('agent')->insert($data);
+    
+//     if ($insert) {
+//         return redirect()->route('backend.agent.show_details', ['usersID' => $usersID])->with('success', 'Agent details added successfully!');
+//         // return redirect()->route('backend.agent.show_details')->with('success', 'Agent details added successfully!');
+//     } else {
+//         $notification = [
+//             'messege' => 'Error creating agent',
+//             'alert-type' => 'error'
+//         ];
+//         return redirect()->route('backend.agent.show_details')->with($notification);
+//     }
+// }
